fc647f0e8bb4bd469aa43f77a6953318127f3b98
jpabloglez <juanpablogglez@gmail.com>
2022-06-17 18:27:27 +0200

    Added axios and integration tests

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -2,11 +2,13 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue'
+//import Tasks from './components/Tasks.vue'
 </script>
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + Vite" />
+  <!--<Tasks msg="Hello Vue 3 + Vite" />-->
 </template>
 
 <style>
--- src/frontend/src/main.js
+++ src/frontend/src/main.js
@@ -1,4 +1,17 @@
 import { createApp } from 'vue'
+//import axios from 'axios'
+//import VueAxios from 'vue-axios'
 import App from './App.vue'
 
+//import Vue from 'vue'
+import axios from 'axios'
+//import VueAxios from 'vue-axios'
+
+//const app = createApp(App)
+//app.use(VueAxios, axios) // 👈
+//app.mount('#app')
 createApp(App).mount('#app')
+
+// set a prototype for http
+Vue.prototype.$http = axios;
+
